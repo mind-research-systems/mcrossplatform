13352c4ca53728e1950a70782a87e8d59cff198c
Donat Müller <donat@mrs.ch>
2017-07-18 11:15:20 +0200

    #2 try download j2objc

---
+++ install_j2objc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+curl -L -o j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
+unzip -q j2objc.zip
+rm -rdf j2objc
+mv j2objc-2.0.1 j2objc
+rm j2objc.zip
