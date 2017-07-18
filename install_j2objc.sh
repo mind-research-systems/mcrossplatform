0fead22a1adca781dc7c08d6484596a91ebc8ff6
Donat Müller <donat@mrs.ch>
2017-07-18 13:32:37 +0200

    #2 revert caching steps

--- install_j2objc.sh
+++ install_j2objc.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-if [ ! -d "j2objc" ]; then
-	# Download j2objc if not exists
-	set -e
-	curl -L -o j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
-	unzip -q j2objc.zip
-	rm -rf j2objc
-	mv j2objc-2.0.1 j2objc
-	rm j2objc.zip
-fi
+set -e
+curl -L -o j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
+unzip -q j2objc.zip
+rm -rf j2objc
+mv j2objc-2.0.1 j2objc
+rm j2objc.zip
