d49a4436a48da0123a9e8cc863b483cbc603500a
Donat Müller <donat@mrs.ch>
2017-07-21 00:06:37 +0200

    #4 fix unzip cmd

--- install_j2objc.sh
+++ install_j2objc.sh
@@ -3,8 +3,8 @@ if [ ! -d "$HOME/j2objc" ]; then
 	echo $HOME/j2objc not found -> download j2objc.zip to $HOME
 	set -e
 	curl -L -o $HOME/j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
-	unzip -q $HOME/j2objc.zip
-	rm -rf -d $HOME $HOME/j2objc
+	unzip -d $HOME $HOME/j2objc.zip
+	rm -rf $HOME/j2objc
 	mv $HOME/j2objc-2.0.1 $HOME/j2objc
 	rm $HOME/j2objc.zip
 else
