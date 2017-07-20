816de3692f46dcd216c0904dbd1c6dd670932a01
Donat Müller <donat@mrs.ch>
2017-07-20 23:57:48 +0200

    #4 try optimize cache
    - put j2objc in home folder
    - exclude mcrossplatform from .m2 caching

--- install_j2objc.sh
+++ install_j2objc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [ ! -d "j2objc" ]; then
-	# Download j2objc if not exists
+if [ ! -d "$HOME/j2objc" ]; then
+	echo $HOME/j2objc not found -> download j2objc.zip to $HOME
 	set -e
-	curl -L -o j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
-	unzip -q j2objc.zip
-	rm -rf j2objc
-	mv j2objc-2.0.1 j2objc
-	rm j2objc.zip
+	curl -L -o $HOME/j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
+	unzip -q $HOME/j2objc.zip
+	rm -rf $HOME/j2objc
+	mv $HOME/j2objc-2.0.1 $HOME/j2objc
+	rm $HOME/j2objc.zip
+else
+	echo found $HOME/j2objc -> no download	
 fi
