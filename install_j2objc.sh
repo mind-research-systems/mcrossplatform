6f805d917e41a2895d086f552137ad7af371ef73
Donat Müller <donat@mrs.ch>
2017-07-21 00:22:41 +0200

    #4 detect j2objc script not install folder

--- install_j2objc.sh
+++ install_j2objc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ ! -d "$HOME/j2objc" ]; then
+if [ ! -f "$HOME/j2objc/j2objc" ]; then
 	echo $HOME/j2objc not found -> download j2objc.zip to $HOME
+	rm -rf $HOME/j2objc
 	set -e
 	curl -L -o $HOME/j2objc.zip https://github.com/google/j2objc/releases/download/2.0.1/j2objc-2.0.1.zip
 	unzip -d $HOME $HOME/j2objc.zip
